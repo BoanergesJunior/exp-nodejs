8aab186f9ed61bd51d0578c2c3eaf2d75c4af22a
Boanerges Junior <boanerges_junior@hotmail.com>
2023-01-29 20:36:24 -0300

    Update order created

--- node-project-02/src/routes/index.js
+++ node-project-02/src/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { readFileData, writeFileData } from '../helper/file.js';
-import '../helper/state.js'
+import '../helper/initial.state.js'
 
 const router = new express.Router()
 
@@ -24,6 +24,37 @@ router.post('/create', async (req, res) => {
   await writeFileData(data);
 
   return res.status(200).send(newOrder)
+})
+
+router.put('/update/:id', async (req, res) => {
+  const { id } = req.params
+  const { client, product, value, delivered } = req.body
+
+  const data = await readFileData();
+
+  let changed = false
+
+  const newGrades = data.grades.map((item) => {
+    if (item.id === Number(id)) {
+      changed = true;
+
+      return {
+        ...item,
+        student: client,
+        subject: product,
+        value,
+        delivered,
+      }
+    }
+
+    return item
+  })
+
+  if (!changed) return res.status(404).send({ message: `Order ${id} not found` })
+
+  await writeFileData({ ...data, grades: newGrades })
+
+  return res.status(200).send(true)
 })
 
 export default router;
